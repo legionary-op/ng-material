7074f29f321cac27242cee279fbc9a3327601a40
Pranay <60820267+legionary-op@users.noreply.github.com>
2020-02-08 23:23:32 +0530

    added login, signup, landing component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, SignupComponent, LandingComponent],
   imports: [
     CommonModule
   ]
