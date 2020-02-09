2f4ae269f709cd23f433e8055e5f3d4327bb487e
Pranay <60820267+legionary-op@users.noreply.github.com>
2020-02-09 21:47:19 +0530

    added component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent],
   imports: [
     CommonModule
   ]
