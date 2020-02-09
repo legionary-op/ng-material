624e64ae7239e35dfc93e74ac12d5f18c1715c93
Pranay <60820267+legionary-op@users.noreply.github.com>
2020-02-09 21:29:42 +0530

    updated the core-routing module.ts

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { LandingComponent } from './landing/landing.component';
 
 
-
 @NgModule({
-  declarations: [LoginComponent, SignupComponent, LandingComponent],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
+
 export class CoreRoutingModule { }
+
+export const CoreRoutingComponents = [
+  LoginComponent,
+  SignupComponent,
+  LandingComponent
+]
